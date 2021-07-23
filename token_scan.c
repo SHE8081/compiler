e4606b99413ba482a20a6fe0dd563b31989aba25
SHE8081 <benben8081@hotmail.com>
2021-07-23 23:57:34 +0800

    ignore

--- token_scan.c
+++ token_scan.c
@@ -1,10 +1,26 @@
 #include<stdio.h>
-/*
 
+/*
+  结构体符号表
     
 **/
 
 
-typedef struct node{
-    int 
+typedef struct ITEM{
+    char *alias;             //单词名称
+    unsigned int type;          //单词类型
+    unsigned int address;       //单词的值
+} 
+
+typedef struct SYMBOL_TABLE{    //符号表
+    struct ITEM  *prior;
+     ITEM  data;
+    struct ITEM *next; 
+}
+
+int initial_symbol_table();    //初始化符号表：1成功；2失败
+
+int initial_symbol_table(FLLE *p){
+
+    
 }
