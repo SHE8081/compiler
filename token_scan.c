21ce4f923d68b5c12125a5f54a4209b36ed67996
SHE8081 <benben8081@hotmail.com>
2021-07-30 11:57:08 +0800

    xx

--- token_scan.c
+++ token_scan.c
@@ -5,7 +5,7 @@
     
 **/
 #define LINE_SIZE 256   //按行读取文件时，每行的最大长度byte
-#define FILEPATH "worldtable"
+#define FILEPATH "./worldtable"
 
 typedef struct ITEM{
     char *alias;             //单词名称
@@ -23,16 +23,25 @@ typedef struct SYMBOL_TABLE{    //符号表
 int initial_symbol_table(){
     char *buf[LINE_SIZE];  
     FILE *p = NULL;
-    p = fopen(FILEPATH,"r");
-    if(p== NULL){
-        printf("ERROR:open file failure!");
+    p = fopen("./wordtable","r");
+    if(p != NULL){
+        fgets(buf,LINE_SIZE-1,p)!= NULL;    //跳过文件第一行
+        while(fgets(buf,LINE_SIZE-1,p)!= NULL){
+            printf ("%s",buf);
+        }
+        fclose(p);
+    }else{
+       printf("ERROR:open file failure!");
         return 0;
     }
+}
+
+int initial_List(){
     
-    while(fgets(buf,LINE_SIZE-1,p)!= NULL){
-        printf ("%s",buf);
-    }
-    fclose(p);
+}
+
+int initial_Node(char *buf){    //从文件每行读取的内容初始化结点
+    while 
 }
 
 int initial_symbol_table(void);
